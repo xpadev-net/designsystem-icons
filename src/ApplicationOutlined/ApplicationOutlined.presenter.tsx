7237543bcbe99c716f052feffad1cfab9a01cc55
xpa <xpadev@gmail.com>
2023-06-28 21:38:11 +0900

    fix: ApplicationOutlinedIcon style

--- src/ApplicationOutlined/ApplicationOutlined.presenter.tsx
+++ src/ApplicationOutlined/ApplicationOutlined.presenter.tsx
@@ -18,7 +18,6 @@ export const ApplicationOutlinedIcon = ({ className, color }: IconProps) => {
           fillRule="evenodd"
           clipRule="evenodd"
           d="M4 2H16.7L20 5.2V22H4V2ZM5.5 3.5V20.5H18.5V6.5H15.5V3.5H5.5ZM7 11H17V12.5H7V11ZM17 8H7V9.5H17V8ZM9.39844 15.1999L11.2984 16.8999L14.5984 13.8999L15.5984 15.0999L11.2984 18.9999L8.39844 16.2999L9.39844 15.1999Z"
-          fill="#1A1A1C"
         />
       </g>
       <defs>
